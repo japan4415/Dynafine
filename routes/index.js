e546ed0ce70d220527e2690e56c850b5d5de077f
discord4415 <raiden000discord@gmail.com>
2015-12-01 05:23:30 +0900

    change can access test.ejs

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
 
 router.get('/dynafine',function(req,res,next){
   console.log('チェックまでは遠てるで');
-  res.render('testhtmls/test',{title:'render test'});
+  res.render('test',{title:'render test'});
 });
 
 module.exports = router;
