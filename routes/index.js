ef22f010c925fb6b4016c582d515bf42529b95cb
discord4415 <raiden000discord@gmail.com>
2015-12-01 05:29:48 +0900

    change can access test.ejs

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log('homeは通過しているよ');
   res.render('index', { title: 'Express tesssts' });
 });
 
