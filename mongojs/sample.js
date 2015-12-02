91d69aa8234c4a27f33f4fcbf920edf78a0eb218
discord4415 <raiden000discord@gmail.com>
2015-12-02 13:51:41 +0900

    ランキング集計

--- mongojs/sample.js
+++ mongojs/sample.js
@@ -1,3 +1,5 @@
-var result = db.wordsapi.find();
+var result = db.wordsapi.aggregate([
+  {$group:{_id:{id:'$siteID',sitename:'$sitename',kanren:'$kanren'},c:{$sum:1}}}
+]);
 DBQuery.shellBatchSize = result.count();
 result
